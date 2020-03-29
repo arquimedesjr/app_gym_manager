41960f35d7ba2a989e7bb4911861c2853c5a5914
kauealvs <kauealvesdesena@gmail.com>
2020-03-29 19:35:59 -0300

    adicionando o botão editar e o crispy forms

--- app_gym_manager/settings.py
+++ app_gym_manager/settings.py
@@ -36,7 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
+
+#Crispy forms
+CRISPY_TEMPLATES_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
