01ab4a866679f1426077ff6f23d18e2d58da171b
Luan Rodrigues <luanlrs96@gmail.com>
2020-04-14 21:03:40 -0300

    Ajustes no form de edição de ficha

--- app/views/ficha_medica_views.py
+++ app/views/ficha_medica_views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from ..forms import AlunoForm, CadastroAvaliacaoFisicaAluno, EditarFichaMedicaAluno
-from ..models import Aluno
+from ..models import Aluno, Ficha_fisica
 
 @login_required
 def cadastrar_avaliacao_fisica(request, template_name='partials/alunos/add-avaliacao-fisica.html'):
@@ -21,12 +21,13 @@ def cadastrar_avaliacao_fisica(request, template_name='partials/alunos/add-avali
 @login_required
 def editar_avaliacao(request, pk, template_name='partials/alunos/edit-avaliacao-fisica.html'):
     form =  EditarFichaMedicaAluno(request.POST)
+    idficha = Ficha_fisica.objects.get(pk=pk)
     if request.method == "POST":
         if form.is_valid():
             form.save()
-            redirect('editar_avaliacao')
+            redirect('/editar-avaliacao/{0}'.format(pk))
         else:
             messages.error(request, 'Os dados foram inseridos de forma incorreta.')
-            redirect('editar_avaliacao')
+            redirect('/editar-avaliacao/{0}'.format(pk))
     # form = Ficha_fisica.object.get(pk=pk)
     return render(request, template_name, {'form': form })
